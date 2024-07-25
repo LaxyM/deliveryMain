fe836faec8277513e8825aa604722e0cd72281e4
AnzhelikaLAV <ztlav2023@gmail.com>
2024-07-25 11:12:08 +0200

    add footer and AboutUs

---
+++ src/components/ButtonViewAll/ButtonViewAll.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ButtonViewAll.css'
+
+const ButtonViewAll = () => {
+	return (
+		<div>
+			<button className='btn'>View all</button>
+		</div>
+	)
+}
+
+export default ButtonViewAll
