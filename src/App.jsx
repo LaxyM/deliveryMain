fa2d9c602cc83246274dbe2df9e6590a93a9f309
AnzhelikaLAV <ztlav2023@gmail.com>
2024-07-28 11:18:11 +0200

    add loginForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import MoreRestaurants from "./pages/MoreRestaurants/MoreRestaurants";
+import Login from "./components/Login/Login";
+
 
 function App() {
-  return (
+  const [showLogin,setShowLogin] = useState(false)
+  return ( 
     <>
+    {showLogin?<Login setShowLogin={setShowLogin}/>:<></>}
       <header className="main-header">
         <div className="navbar-container">
-          <Navbar />
+          <Navbar setShowLogin={setShowLogin}/>
         </div>
       </header>
       <div className="app">
